b2c05dab0d27d70e3e19b2db7fd166b86576b1aa
Charlie Hölscher <charlieholscher@gmail.com>
2022-02-14 21:18:50 +0100

    attempt at bonus

--- js/index.js
+++ js/index.js
@@ -57,3 +57,41 @@ for (let j = 0; j < hacker1.length; j++)
 
 // bonus time!
 
+let paragraph1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies sagittis augue, ut ultrices lectus dapibus in. Duis auctor metus enim. Curabitur vel suscipit tortor. Ut at enim at massa laoreet luctus placerat tempor orci. Aliquam augue libero, efficitur congue rhoncus et, hendrerit a quam. Aenean ullamcorper pretium nisl, ac imperdiet justo tristique ac. Curabitur a dui mattis, ullamcorper elit quis, consequat enim. Etiam id fermentum quam.";
+let paragraph2 = "Nullam diam eros, finibus nec purus eget, pellentesque sodales neque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus malesuada eros at euismod fringilla. Vivamus velit enim, viverra sed enim et, elementum mollis leo. Phasellus ex neque, dapibus sit amet euismod tincidunt, sodales vel mi. Suspendisse ornare lectus in ante luctus dignissim. Proin finibus ullamcorper lacus tincidunt sagittis. Suspendisse porta sed risus sed vestibulum. Etiam commodo dolor ut quam rhoncus sodales.";
+let paragraph3 = "Proin ac dui venenatis, interdum neque sit amet, consectetur lacus. Aliquam ultrices eros ac velit imperdiet tristique. Donec a libero nec ligula porta congue. Sed eget pharetra lorem. Integer venenatis non est quis posuere. In non feugiat leo. Praesent euismod dui orci. Donec lobortis dui eget nunc malesuada mattis.";
+let allParagraphs = paragraph1 + paragraph2 + paragraph3;
+
+
+for(let i = 0; i <= allParagraphs.length; i++){
+    console.log(`the amount of words are: ${allParagraphs.split(" ").length}`); 
+    {break} // this is not actually the correct amount of words according to googledrive word count, but i cannot figure out why.
+}
+
+
+/* same as above but in function version, which i found help with first online:
+
+function wordcount1(){
+    return allParagraphs.split(" ").length;
+}
+
+console.log(wordcount1(allParagraphs));
+*/
+
+let etCounter = "";
+for(let i = 0; i <= allParagraphs.length; i++){
+    if(allParagraphs[0] == "e" && allParagraphs[0]+1 == "t" && allParagraphs[0]+2 == " "){
+    etCounter += allParagraphs[i];
+
+    console.log(allParagraphs[i]);}
+}
+
+/* same as above but in function version, which i found help with first online:
+
+function countEt(){
+    return (allParagraphs.split(' et ')).length -1; ---> the internet told me to add -1 but whyyyyy??
+}
+
+console.log(countEt(allParagraphs));
+
+*/
